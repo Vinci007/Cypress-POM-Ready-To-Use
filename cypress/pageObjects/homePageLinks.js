d5cead6a3c873be30bdd3173a17e52e59477cf90
Vinci007 <vinfer07@gmail.com>
2023-05-17 00:39:58 +0530

    Check

--- cypress/pageObjects/homePageLinks.js
+++ cypress/pageObjects/homePageLinks.js
@@ -1,7 +1,7 @@
 class homePageLinks {
 
    homepage() {
-      return cy.visit('https://www.cypress.io/')
+      return cy.visit('https://www.amazon.com')
    }
    howitworkspagelink() {
       return cy.get('.styled__MenuWrapper-sc-16oj5lj-1 > .styled__NavList-sc-16oj5lj-3:nth-child(1) > .styled__NavItem-sc-16oj5lj-4:nth-child(2) > .Link-sc-5cc5in-0')
